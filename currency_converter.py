8d8bc959bdc395402582732c4be6c1c7303934f7
project <nikitasengurov@MacBook-Air-Nikita.local>
2026-01-21 16:02:43 +0300

    Add history with JSON

--- currency_converter.py
+++ currency_converter.py
@@ -1,46 +1,51 @@
-continue_choice = 'y'
-
+import json
 history =[]
+try:
+    with open('history.json', 'r') as file:
+        data = json.load(file)
+        if isinstance(data, list):
+            history = data
+        else:
+            history = []
+except (FileNotFoundError, json.JSONDecodeError, ValueError):
+    history = []
+
+exchange_currency = {
+    'USD': 90,
+    'EUR': 98,
+    'CNY': 12.5
+}
+
+def save_history():
+    with open('history.json', 'w') as file:
+        json.dump(history, file, indent=2)
+
 while True:
-    if continue_choice == 'y':
+    try:
         money_input = int(input('Здравствуйте, введите сумму в рублях (RUB): '))
-        currency = input("Спасибо! Введите валюту для конвертации (USD, EUR, CNY): ").upper()
-        match currency:
-            case 'USD':
-                result = money_input / 90
-                print(f"{money_input} RUB = {result:.2f} {currency}")
-                history.append({
-                    'amount_rub': money_input,
-                    'cur': currency,
-                    'result': result
-                })
-                continue_choice = input('Хотите продолжить? Если да введите \'y\' иначе \'n\': ')
-            case 'EUR':
-                result = money_input / 98
-                print(f"{money_input} RUB = {result:.2f} {currency}")
-                history.append({
-                    'amount_rub': money_input,
-                    'cur': currency,
-                    'result': result
-                })
-                continue_choice = input('Хотите продолжить? Если да введите \'y\' иначе \'n\': ')
-            case 'CNY':
-                result = money_input / 12.5
-                print(f"{money_input} RUB = {result:.2f} {currency}")
-                history.append({
-                    'amount_rub': money_input,
-                    'cur': currency,
-                    'result': result
-                })
-                continue_choice = input('Хотите продолжить? Если да введите \'y\' иначе \'n\': ')
-            case _:
-                print('Ошибка: неизвестная валюта')
-                print("Повторите попытку")
-                continue
-    else:
+    except ValueError:
+        print("Ошибка введите целое число")
+        continue
+    if money_input <= 0:
+        print("Сумма должна быть больше 0")
+        continue
+    currency = input("Спасибо! Введите валюту для конвертации (USD, EUR, CNY): ").upper()
+    if currency not in exchange_currency:
+        print("Данная валюта не поддерживается, попробуйте еще раз)")
+        continue
+    rate_of_currency = exchange_currency[currency]
+    result = money_input / rate_of_currency
+    print(f"{money_input} RUB = {result:.2f} {currency}")
+    history.append({
+        'amount_rub': money_input,
+        'cur': currency,
+        'result': result
+    })
+    save_history()
+    continue_choice = input("Хотите продолжить? Если да введите \'y\' иначе \'n\': ").lower()
+    if continue_choice != 'y':
         break
 
 print(f'История конвертаций:')
 for i in history:
     print(f"{i['amount_rub']} RUB = {i['result']:.2f} {i['cur']}")
-
